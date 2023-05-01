64e7a546160309dc56390529805f06933c1b10a3
yannic310 <scheyvaertsyannic@gmail.com>
2023-05-01 18:12:14 +0200

    timeline2 werkt

--- src/Charts/TimeLine2.js
+++ src/Charts/TimeLine2.js
@@ -1,21 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+import * as vis from 'vis-timeline';
+import 'vis-timeline/dist/vis-timeline-graph2d.min.css';
 
-import { DataSet } from 'vis-data';
-import { Timeline } from 'vis-timeline';
+const TimeLine2 = () => {
+  const timelineRef = useRef(null);
 
-var items = new DataSet([
-  {id: 1, content: 'item 1', start: '2014-04-20'},
-  {id: 2, content: 'item 2', start: '2014-04-14'},
-  {id: 3, content: 'item 3', start: '2014-04-18'},
-  {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-  {id: 5, content: 'item 5', start: '2014-04-25'},
-  {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
-]);
+  useEffect(() => {
+    const container = timelineRef.current;
+    const data = [
+      { id: 1, content: 'item 1', start: '2013-04-20' },
+      { id: 2, content: 'item 2', start: '2013-04-14' },
+      { id: 3, content: 'item 3', start: '2013-04-18' },
+      { id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19' },
+      { id: 5, content: 'item 5', start: '2013-04-25' },
+      { id: 6, content: 'item 6', start: '2013-04-27' },
+    ];
+    const options = {};
 
-var container = document.getElementById('visualization');
+    const timeline = new vis.Timeline(container, data, options);
 
-// Configuration for the Timeline
-var options = {};
+    return () => {
+      timeline.destroy();
+    };
+  }, []);
 
-// Create a Timeline
-var TimeLine2 = new Timeline(container, items, options);
+  return <div ref={timelineRef} />;
+};
+
 export default TimeLine2;
